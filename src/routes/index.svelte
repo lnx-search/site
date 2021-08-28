32a1b5a50d2acd990cdd1da8d568504ad221e6e7
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-08-28 19:27:22 +0100

    apply latest changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,33 +1,16 @@
 <script>
 	import Typewriter from 'svelte-typewriter';
-    import {fade} from 'svelte/transition';
-    import {onMount} from 'svelte';
-
-    let target = "fast"
-
-    function setFast() {
-        target = "fast";
-        setTimeout(setFriendly, 2400);
-
-    }
-
-    function setFriendly() {
-        target = "friendly";
-        setTimeout(setLnx, 3200);
-
-    }
-
-    function setLnx() {
-        target = "lnx";
-        setTimeout(setFast, 1900);
-    }
-
-    onMount(setFast);
 </script>
 
-<div class="flex flex-col pt-32 min-h-screen overflow-x-hidden">
-    <div class="flex justify-around xl:justify-center flex-wrap px-16">
-        <div class="h-64 w-1/2 mt-20">
+<div class="flex flex-col items-center pt-16 min-h-screen overflow-x-hidden">
+    <div class="flex flex-col items-center">
+        <h1 class="text-8xl font-bold italic text-indigo-600">
+            lnx
+        </h1>
+        <h3 class="text-2xl font-bold italic">An adaptable search engine API.</h3>
+    </div>
+    <div class="flex items-center justify-around xl:justify-center flex-wrap px-16 mt-24">
+        <div class="h-64 w-1/2">
             <div class="text-4xl lg:text-6xl font-bold border-l-4 border-indigo-600 pt-1 pb-3 pl-4">
                 Make it
                 <div class="flex text-7xl ml-8">
@@ -36,7 +19,7 @@
                         <Typewriter loop interval={80}>
                             <span>lightning fast.</span>
                             <span>relevant.</span>
-                            <span>typo-tolerant.</span>
+                            <span>typo tolerant.</span>
                             <span>search as you type.</span>
                             <span>your way.</span>
                         </Typewriter>
@@ -45,11 +28,9 @@
             </div>
             <div class="mt-16">
                 <p class="text-3xl font-semibold w-4/5">
-                    Create projects your way with <span class="italic font-bold text-7xl text-indigo-600">lnx</span>.
-                    <br/>
                     An open-source,
                     <span class="italic border-b-2 border-indigo-600">lightning-fast</span>
-                    and <span class="italic border-b-2 border-indigo-600">typo-tolerant</span> search engine
+                    and <span class="italic border-b-2 border-indigo-600">typo tolerant</span> search engine
                     empowering developers and users alike.
                 </p>
             </div>
@@ -74,7 +55,7 @@
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                 </svg>
                 <div class="flex flex-col w-3/4">
-                    <p class="text-xl font-bold italic">Typo-Tolerant</p>
+                    <p class="text-xl font-bold italic">Typo Tolerant</p>
                     <p>lnx provides fuzzy query systems to give you a typo-tolerant query.</p>
                 </div>
             </div>
@@ -87,7 +68,7 @@
                     <p>Currently lnx supports 17 latin languages. Chinese, japanese and korean are planned.</p>
                 </div>
             </div>
-            <div class="transform rotate-3 flex justify-around items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4" style="width: 28rem">
+            <div class="flex justify-around items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4" style="width: 28rem">
                 <a href="https://book.lnx.rs" class="cursor-pointer flex items-center justify-center hover:shadow-inner transform duration-200 text-center text-white text-2xl font-bold border-2 border-black bg-indigo-400 hover:bg-indigo-500 rounded-lg h-12 pb-1 px-4">
                     🕹️ Source
                 </a>
