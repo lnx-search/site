3f40ecf12595c55030f1b021a12d424c49f7ec02
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-08-28 20:31:53 +0100

    make it mobile friendly

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,17 +3,17 @@
 </script>
 
 <div class="flex flex-col items-center pt-16 min-h-screen overflow-x-hidden">
-    <div class="flex flex-col items-center">
+    <div class="flex flex-col items-center px-4">
         <h1 class="text-8xl font-bold italic text-indigo-600">
             lnx
         </h1>
-        <h3 class="text-2xl font-bold">An adaptable search engine API.</h3>
+        <h3 class="text-2xl text-center font-bold">An adaptable search engine API.</h3>
     </div>
-    <div class="flex justify-around xl:justify-center flex-wrap px-16 mt-24">
-        <div class="h-64 w-1/2 mt-24">
-            <div class="text-4xl lg:text-6xl font-bold border-l-4 border-indigo-600 pt-1 pb-3 pl-4">
+    <div class="flex flex-col items-center lg:items-start lg:flex-row md:justify-between xl:justify-center flex-wrap px-16 my-4 lg:my-24">
+        <div class="flex flex-col items-center lg:items-start w-full lg:w-1/2 mt-12 lg:mt-24">
+            <div class="text-2xl lg:text-6xl font-bold border-l-4 border-indigo-600 pt-1 pb-3 pl-4 w-4/5 lg:w-auto">
                 Make it
-                <div class="flex text-7xl ml-8">
+                <div class="flex text-3xl lg:text-7xl ml-8">
                     >
                     <div class="italic text-indigo-600 ml-4">
                         <Typewriter loop interval={80}>
@@ -26,8 +26,8 @@
                     </div>
                 </div>
             </div>
-            <div class="mt-16">
-                <p class="text-3xl font-semibold w-4/5">
+            <div class="flex justify-center lg:justify-start mt-8 lg:mt-16">
+                <p class="text-3xl font-semibold text-center lg:text-left w-4/5 px-4 lg:px-0">
                     An open-source,
                     <span class="italic border-b-2 border-indigo-600">lightning-fast</span>
                     and <span class="italic border-b-2 border-indigo-600">typo tolerant</span> search engine
@@ -35,44 +35,44 @@
                 </p>
             </div>
         </div>
-        <div class="flex flex-col items-center bg-indigo-600 rounded-lg shadow-inner py-4 mx-4 text-white" style="width: 426px;">
-            <div class="transform rotate-3 flex justify-center items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4" style="width: 28rem">
+        <div class="flex flex-col items-center bg-indigo-600 rounded-lg shadow-inner py-4 mx-4 text-white mt-8 lg:mt-0 w-9/12 lg:w-426">
+            <div class="transform rotate-3 flex justify-center items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4 w-80 lg:w-28">
                 <img class="w-12 h-12 mr-4" src="/love.svg" alt="">
                 <div class="flex flex-col w-3/4">
-                    <p class="text-xl font-bold italic">Community Driven</p>
-                    <p>100% open-source, built for the community.</p>
+                    <p class="text-base lg:text-xl font-bold italic">Community Driven</p>
+                    <p class="text-sm lg:text-base">100% open-source, built for the community.</p>
                 </div>
             </div>
-            <div class="transform -rotate-3 flex justify-center items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4" style="width: 28rem">
+            <div class="transform -rotate-3 flex justify-center items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4 w-80 lg:w-28">
                 <img class="w-12 h-12 mr-4" src="/lightning.svg" alt="">
                 <div class="flex flex-col w-3/4">
-                    <p class="text-xl font-bold italic">Insanely Fast</p>
-                    <p>Query millions of documents in milliseconds with our fast-fuzzy system.</p>
+                    <p class="text-base lg:text-xl font-bold italic">Insanely Fast</p>
+                    <p class="text-sm lg:text-base">Query millions of documents in milliseconds with our fast-fuzzy system.</p>
                 </div>
             </div>
-            <div class="transform rotate-3 flex justify-around items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4" style="width: 28rem">
+            <div class="transform rotate-3 flex justify-around items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4 w-80 lg:w-28">
                 <svg xmlns="http://www.w3.org/2000/svg" class="text-green-400 h-12 w-12 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                 </svg>
                 <div class="flex flex-col w-3/4">
-                    <p class="text-xl font-bold italic">Typo Tolerant</p>
-                    <p>lnx provides fuzzy query systems to give you a typo-tolerant query.</p>
+                    <p class="text-base lg:text-xl font-bold italic">Typo Tolerant</p>
+                    <p class="text-sm lg:text-base">lnx provides fuzzy query systems to give you a typo-tolerant query.</p>
                 </div>
             </div>
-            <div class="transform -rotate-3 flex justify-center items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4" style="width: 28rem">
+            <div class="transform -rotate-3 flex justify-center items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4 w-80 lg:w-28">
                 <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-12 w-12 mr-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
                 </svg>
                 <div class="flex flex-col w-3/4">
-                    <p class="text-xl font-bold italic">Multi-Language</p>
-                    <p>Currently lnx supports 17 latin languages. Chinese, japanese and korean are planned.</p>
+                    <p class="text-base lg:text-xl font-bold italic">Multi-Language</p>
+                    <p class="text-sm lg:text-base">Currently lnx supports 17 latin languages. Chinese, japanese and korean are planned.</p>
                 </div>
             </div>
-            <div class="flex justify-around items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4" style="width: 28rem">
-                <a href="https://book.lnx.rs" class="cursor-pointer flex items-center justify-center hover:shadow-inner transform duration-200 text-center text-white text-2xl font-bold border-2 border-black bg-indigo-400 hover:bg-indigo-500 rounded-lg h-12 pb-1 px-4">
+            <div class="flex justify-around items-center h-24 bg-gray-900 rounded-lg shadow-md my-4 p-4 w-80 lg:w-28">
+                <a href="https://book.lnx.rs" class="cursor-pointer flex items-center justify-center hover:shadow-inner transform duration-200 text-center text-white text-base lg:text-2xl font-bold border-2 border-black bg-indigo-400 hover:bg-indigo-500 rounded-lg h-12 pb-1 px-4">
                     🕹️ Source
                 </a>
-                <a href="https://book.lnx.rs" class="cursor-pointer flex items-center justify-center hover:shadow-inner transform duration-200 text-center text-white text-2xl font-bold border-2 border-black bg-indigo-400 hover:bg-indigo-500 rounded-lg h-12 pb-1 px-4">
+                <a href="https://book.lnx.rs" class="cursor-pointer flex items-center justify-center hover:shadow-inner transform duration-200 text-center text-white text-base lg:text-2xl font-bold border-2 border-black bg-indigo-400 hover:bg-indigo-500 rounded-lg h-12 pb-1 px-4">
                     🚀 Get Started
                 </a>
             </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,13 @@ module.exports = {
     purge: ['src/app.html', 'src/**/*.{svelte,css}'],
     darkMode: false, // or 'media' or false
     theme: {
-        extend: {},
+        extend: {
+            width: {
+                '426': '426px',
+                '28': '28rem',
+                '80': '20rem',
+            }
+        },
     },
     variants: {},
     plugins: [],
